1df25f1daf1d6b0c1f85f88761dff6256546fdd7
axing <1253740807@qq.com>
2022-07-27 22:41:50 +0800

    header 简单样式完成

--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, PropType } from "vue";
+import { Icon } from "../shared/icon";
 import s from "./Header.module.scss";
 export const Header = defineComponent({
   // props: {
@@ -7,9 +8,26 @@ export const Header = defineComponent({
   //   },
   // },
   setup: (props, context) => {
-    return () => <div class={s.wrapper}>
-      <div class="title">天津市突发事件预警信息发布系统</div>
-      <div class="content">heheda</div>
-      </div>;
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.title}>
+          <svg>
+            <use xlinkHref="#qrcode"></use>
+          </svg>
+          <div class={s.titleContent}>天津市突发事件预警信息发布系统</div>
+        </div>
+        <div class={s.content}>
+          <div>
+            <Icon name="waitting" />
+            <span>代办事项</span>
+          </div>
+          <div>
+            <Icon name="tongzhi" />
+            <span>通知</span>
+          </div>
+          <div>气象局</div>
+        </div>
+      </div>
+    );
   },
 });
